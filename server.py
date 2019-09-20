29745bfbe4a1d6572263acb9256ac2b403c5330d
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-09-20 12:41:23 +1000

    add colour route

--- server.py
+++ server.py
@@ -5,6 +5,21 @@ app = Flask(__name__)
 POKEMON_API_URL = "https://pokeapi.co/api/v2/"
 COLOUR_URL = POKEMON_API_URL + 'pokemon-color'
 
+@app.route('/<colour>') 
+def get_by_colour(colour):
+    colour_json = requests.get(f'{COLOUR_URL}/{colour}').json()
+    pokemon_species_list = colour_json["pokemon_species"]
+
+    html = ''
+
+    for species in pokemon_species_list:
+        html += f'<p>{species["name"]}</p>'
+        # print(species["name"])
+
+    # colour_json["pokemon_species"][0]["name"]
+    return html
+    
+
 @app.route('/')
 def index():
     colours_json = requests.get(COLOUR_URL).json()
